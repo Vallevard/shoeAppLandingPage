da68977b7978130a7373194eed828c2ab8f23252
Vallevard <vallevard@gmail.com>
2021-10-11 16:49:28 -0500

    updated

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # ShoeApp Landing Page
+1- git clone https://github.com/Vallevard/shoeAppLandingPage.git
+2- cd "appname"
+3- npm install
+4- gulp
+
+
 The code files for the responsive landing page project.
 
 Check out [The YouTube series to see my process](https://www.youtube.com/watch?v=sr6jDeAoXCc&feature=youtu.be&list=PLrtjkLnNjGHu7QIc8jN7hZmuP6wMby2QZ) for more info.
--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,83 @@
-// --------------------------------------------
-// Dependencies
-// --------------------------------------------
-var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    del = require('del'),
-    minifycss = require('gulp-minify-css'),
-    plumber = require('gulp-plumber'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    images = require('gulp-imagemin'),
-    browserSync = require('browser-sync').create();
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const browsersync = require('browser-sync').create();
 
 
-// paths
-var styleSrc = 'source/sass/**/*.sass',
-    styleDest = 'build/assets/css/',
-    htmlSrc = 'source/',
-    htmlDest = 'build/',
-    vendorSrc = 'source/js/vendors/',
-    vendorDest = 'build/assets/js/',
-    scriptSrc = 'source/js/*.js',
-    scriptDest = 'build/assets/js/';
+let styleSrc = 'source/sass/**/*.sass';
+// let styleDest = 'build/assets/css/';
+
+// let vendorSrc = 'source/js/vendors/';
+// let vendorDest = 'build/assets/js/';
+let scriptSrc = 'source/js/*.js';
+// let scriptDest = 'build/assets/js/';
+
+// let htmlSrc = 'source/';
+// let htmlDest = 'build/';
+
+// Sass Task
+function scssTask(){
+  return src('source/sass/**/*.sass', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sass({
+      style: 'compressed'
+    }))
+    .pipe(postcss([cssnano()]))
+    .pipe(rename({
+        basename: 'main',
+        suffix: '.min'
+    }))
+    .pipe(dest('build/assets/css', { sourcemaps: '.' }));
+}
+
+function imgTask() {
+  src('source/img/*')
+      .pipe(dest('build/assets/img'));
+
+};
 
 
+// JavaScript Task
+function jsTask(){
+  return src('source/js/*.js', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(terser())
+    .pipe(dest('build/assets/js', { sourcemaps: '.' }));
+}
 
-// --------------------------------------------
-// Stand Alone Tasks
-// --------------------------------------------
+// Browsersync Tasks
+function browsersyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: './build'
+    }
+  });
+  cb();
+}
 
+function browsersyncReload(cb){
+  browsersync.reload();
+  cb();
+}
 
-// Compiles all SASS files
-gulp.task('sass', function() {
-    gulp.src('source/sass/**/*.sass')
-    .pipe(sourcemaps.init())
-        .pipe(plumber())
-        .pipe(sass({
-            style: 'compressed'
-        }))
-        .pipe(rename({
-            basename: 'main',
-            suffix: '.min'
-          }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/assets/css'));
-});
+// Watch Task
+function watchTask(){
+  watch('build/*.html', browsersyncReload);
+  watch([styleSrc, scriptSrc], series(scssTask, jsTask, browsersyncReload));
+}
 
-gulp.task('images', function() {
-    gulp.src('source/img/*')
-        .pipe(images())
-        .pipe(gulp.dest('build/assets/img'));
-});
+// Default Gulp task
+exports.default = series(
+  scssTask,
+  jsTask,
+  browsersyncServe,
+  watchTask
+);
 
-// Uglify js files
-gulp.task('scripts', function() {
-    gulp.src('source/js/*.js')
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(gulp.dest('build/assets/js'));
-});
-
-//Concat and Compress Vendor .js files
-gulp.task('vendors', function() {
-    gulp.src(
-            [
-                'source/js/vendors/jquery.min.js',
-                'source/js/vendors/*.js'
-            ])
-        .pipe(plumber())
-        .pipe(concat('vendors.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/assets/js'));
-});
-
-
-
-// Watch for changes
-gulp.task('watch', function(){
-
-    // Serve files from the root of this project
-    browserSync.init({
-        server: {
-            baseDir: "./build"
-        },
-        notify: false
-    });
-
-    gulp.watch(styleSrc,['sass']);
-    gulp.watch(scriptSrc,['scripts']);
-    gulp.watch(vendorSrc,['vendors']);
-    gulp.watch(['build/*.html', 'build/assets/css/*.css', 'build/assets/js/*.js', 'build/assets/js/vendors/*.js']).on('change', browserSync.reload);
-
-});
-
-
-// use default task to launch Browsersync and watch JS files
-gulp.task('default', [ 'sass', 'scripts', 'vendors', 'watch'], function () {});
+exports.img = series(
+  imgTask
+);
